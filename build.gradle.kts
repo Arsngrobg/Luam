8e794820606cf5e94016f0c8eb37556f6035d485
James Armstrong <arsngrobg@gmail.com>
2026-05-30 04:31:09 +0100

    build: configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,19 @@
+subprojects {
+    plugins.apply("java")
 
+    repositories {
+        mavenCentral()
+    }
+
+    the<JavaPluginExtension>().apply {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(25))
+        }
+    }
+
+    // earliest version of java when Mojang introduced datapacks (subject to change)
+    tasks.withType<JavaCompile>().configureEach {
+        options.compilerArgs.add("-Xlint:-options")
+        options.release.set(8)
+    }
+}
