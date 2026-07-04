bb77cd47a7410e08dad73915c84d14d02c10731c
James Armstrong <arsngrobg@gmail.com>
2026-07-04 03:32:32 +0100

    fix(parser): use shorter error message for illegal input

--- luam-parser/src/main/java/dev/arsngrobg/luam/parser/LuaSourcePosition.java
+++ luam-parser/src/main/java/dev/arsngrobg/luam/parser/LuaSourcePosition.java
@@ -2,8 +2,8 @@ package dev.arsngrobg.luam.parser;
 
 public record LuaSourcePosition(int line, int column) {
     public LuaSourcePosition {
-        if (line   < 0) throw new IllegalArgumentException("LuaSourcePosition line must be unsigned");
-        if (column < 0) throw new IllegalArgumentException("LuaSourcePosition column must be unsigned");
+        if (line   < 0) throw new IllegalArgumentException("line must be unsigned");
+        if (column < 0) throw new IllegalArgumentException("column must be unsigned");
     }
 
     @Override
