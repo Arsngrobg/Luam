fce0ce1316e978f528fc1a20271c5b1ccde36ce0
James Armstrong <arsngrobg@gmail.com>
2026-07-04 03:40:56 +0100

    fix(parser): reduction of metadata + more helper methods

--- luam-driver/src/main/java/dev/arsngrobg/luam/driver/LuamDriver.java
+++ luam-driver/src/main/java/dev/arsngrobg/luam/driver/LuamDriver.java
@@ -4,9 +4,10 @@ import dev.arsngrobg.luam.parser.*;
 
 public final class LuamDriver {
     public static void main(String[] args) {
-        LuaSource src1 = LuaSource.ofFile("../examples/1-hello-world.lua").orElseThrow();
-        LuaSource src2 = LuaSource.ofFile("../examples/2-arithmetic.lua").orElseThrow();
-        System.out.println(src1);
-        System.out.println(src2);
+        LuaSource src = LuaSource.ofFile("../examples/2-arithmetic.lua").orElseThrow();
+        System.out.println(src.getContent());
+
+        System.out.println(src.charAt(new LuaSourcePosition(2, 10)));
+        System.out.println(src.eof());
     }
 }
--- luam-parser/src/main/java/dev/arsngrobg/luam/parser/LuaSource.java
+++ luam-parser/src/main/java/dev/arsngrobg/luam/parser/LuaSource.java
@@ -6,74 +6,115 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Optional;
 
-public final class LuaSource {
-    public static final LuaSource EMPTY = new LuaSource(new byte[0]);
+public final class LuaSource implements Iterable<Character> {
+    public static Optional<LuaSource> ofFile(String filepath) {
+        Objects.requireNonNull(filepath, "filepath cannot be NULL");
 
-    public static Optional<LuaSource> ofFile(String filename) {
-        Objects.requireNonNull(filename, "LuaSource::ofFile filename cannot be NULL");
         try {
-            byte[] fileBytes = Files.readAllBytes(new File(filename).toPath());
-            return Optional.of(
-                (fileBytes.length == 0)
-                ? LuaSource.EMPTY
-                : new LuaSource(fileBytes)
-            );
-        } catch (IOException e) { return Optional.empty(); }
+            File file = new File(filepath);
+            byte[] bytes = Files.readAllBytes(file.toPath());
+            return Optional.of(new LuaSource(bytes));
+        } catch (IOException e) {
+            System.err.println(e);
+            return Optional.empty();
+        }
     }
 
     public static LuaSource ofString(String source) {
-        Objects.requireNonNull(source, "LuaSource::ofString source cannot be NULL");
-        byte[] asBytes = source.getBytes(StandardCharsets.UTF_8);
-        return (asBytes.length == 0) ? LuaSource.EMPTY : new LuaSource(asBytes);
+        Objects.requireNonNull(source, "source cannot be NULL");
+        return new LuaSource(source.getBytes(StandardCharsets.UTF_8));
     }
 
     private final byte[] bytes;
-    private final int[]  lineLookup;
 
-    private final LuaSourcePosition eof;
+    private final int[] lineOffsets;
 
-    private LuaSource(byte[] bytes) {
-        this.bytes = Objects.requireNonNull(bytes, "LuaSource bytes cannot be NULL");
+    public LuaSource(byte[] bytes) {
+        this.bytes = Objects.requireNonNull(bytes, "bytes cannot be NULL");
 
         List<Integer> buffer = new ArrayList<>(List.of(0));
-
-        int line = 0, column = 0;
         for (int idx = 0; idx < byteCount(); idx++) {
             char ch = (char) bytes[idx];
-            line   += (ch == '\n') ? 1       : 0;
-            column += (ch == '\n') ? -column : 1;
-
-            if (ch != '\n') continue;
-            buffer.add((idx == byteCount()-1) ? idx : idx+1);
+            if (ch == '\n') {
+                buffer.add(idx+1);
+            }
         }
-
-        lineLookup = buffer.stream()
-                           .mapToInt(Integer::intValue)
-                           .toArray();
-        eof = new LuaSourcePosition(line, column);
+        lineOffsets = buffer.stream().mapToInt(Integer::valueOf).toArray();
     }
 
-    public char charAt(LuaSourcePosition pos) {
-        Objects.requireNonNull(pos, "LuaSource::charAt pos cannot be NULL");
-        int idx = lineLookup[pos.line()] + pos.column();
-        return (char) bytes[idx];
+    public char charAt(LuaSourcePosition position) {
+        Objects.requireNonNull(position, "position cannot be NULL");
+
+        if (position.column() > columnCountForLine(position.line())) {
+            throw new IndexOutOfBoundsException("column position is out of bounds");
+        }
+
+        return (char) bytes[lineOffsets[position.line()] + position.column()];
+    }
+
+    public String getContent() {
+        StringBuilder buffer = new StringBuilder();
+        for (char ch : this) {
+            buffer.append(ch);
+        }
+        return buffer.toString();
     }
 
     public LuaSourcePosition eof() {
-        return eof;
+        return new LuaSourcePosition(lineCount(), columnCountForLine(lineCount()-1));
+    }
+
+    public int columnCountForLine(int line) {
+        if (line <  0)           throw new IllegalArgumentException("line must be unsigned");
+        if (line >= lineCount()) throw new IndexOutOfBoundsException("line is out of bounds");
+
+        int offset = lineOffsets[line];
+        int next   = (line == (lineCount()-1)) ? bytes.length : lineOffsets[line+1];
+        return next - offset;
+    }
+
+    public int lineCount() {
+        return lineOffsets.length;
     }
 
     public int byteCount() {
         return bytes.length;
     }
 
+    public byte[] toByteArray() {
+        return Arrays.copyOf(bytes, bytes.length);
+    }
+
+    @Override
+    public Iterator<Character> iterator() {
+        return new Iterator<Character>() {
+            private int idx = 0;
+
+            @Override
+            public Character next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException("LuaSourceIterator reached end of byte buffer");
+                }
+
+                return (char) bytes[idx++];
+            }
+
+            @Override
+            public boolean hasNext() {
+                return idx != byteCount();
+            }
+        };
+    }
+
     @Override
     public int hashCode() {
-        return Arrays.hashCode(bytes);
+        return bytes.hashCode();
     }
 
     @Override
@@ -82,23 +123,11 @@ public final class LuaSource {
             return false;
         }
 
-        return bytes.equals(src.bytes);
+        return Arrays.equals(bytes, src.bytes);
     }
 
     @Override
     public String toString() {
-        StringBuilder buffer = new StringBuilder("Lua Source (").append(byteCount()).append("B)");
-        if (byteCount() == 0) {
-            return buffer.toString();
-        }
-
-        buffer.append("\n| ");
-        for (int idx = 0; idx < byteCount(); idx++) {
-            char ch = (char) bytes[idx];
-            if (ch == '\n') buffer.append("\n| ");
-            else            buffer.append(ch);
-        }
-
-        return buffer.toString();
+        return String.format("LuaSource[SIZE=%dB,LINES=%d]", byteCount(), lineCount());
     }
 }
