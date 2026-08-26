adf2ebd965dde991e38a5b99e7b61305e495bed3
James Armstrong <arsngrobg@gmail.com>
2026-08-26 03:58:32 +0100

    feat(parser): some more mock commands

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,6 +63,6 @@ project(":luam-driver") {
     }
 
     extensions.configure<JavaApplication> {
-        mainClass.set("dev.arsngrobg.luam.driver.LuamDriverKt")
+        mainClass.set("dev.arsngrobg.luam.driver.LuamMainKt")
     }
 }
---
+++ luam-driver/src/main/kotlin/dev/arsngrobg/luam/driver/LuamMain.kt
@@ -0,0 +1,10 @@
+package dev.arsngrobg.luam.driver
+
+import com.github.ajalt.clikt.core.*
+import com.github.ajalt.mordant.rendering.AnsiLevel
+import com.github.ajalt.mordant.terminal.Terminal
+
+fun main(argv: Array<String>): Unit =
+    LuamCommand().context {
+        terminal = Terminal(AnsiLevel.TRUECOLOR, interactive = true)
+    }.main(argv)
