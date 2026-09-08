59d1959f3051f8363db9e43e33f8e6a5af931262
James Armstrong <arsngrobg@gmail.com>
2026-09-08 20:36:30 +0100

    :art: remove wildcard imports

--- luam-driver/src/main/kotlin/io/github/arsngrobg/luam/driver/LuamMain.kt
+++ luam-driver/src/main/kotlin/io/github/arsngrobg/luam/driver/LuamMain.kt
@@ -1,6 +1,8 @@
 package io.github.arsngrobg.luam.driver
 
-import com.github.ajalt.clikt.core.*
+import com.github.ajalt.clikt.core.context
+import com.github.ajalt.clikt.core.terminal
+import com.github.ajalt.clikt.core.main
 import com.github.ajalt.mordant.rendering.AnsiLevel
 import com.github.ajalt.mordant.terminal.Terminal
 
